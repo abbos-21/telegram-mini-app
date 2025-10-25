27a60882653a732094180b67cb197627f5ccbf27
abbos-21 <abbos.nazariy0621@gmail.com>
2025-10-25 21:48:59 +0500

    commit 27

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -10,10 +10,16 @@ import { io } from 'socket.io-client'
 
 const URL = 'http://localhost:8080' // Address of your Node.js server
 
+const token = localStorage.getItem('token')
+console.log(token)
+
 // Create the Socket.IO client instance
 export const socket = io(URL, {
   autoConnect: true,
-  transports: ['websocket', 'polling'], // Recommended transports for production
+  transports: ['websocket', 'polling'],
+  auth: {
+    token,
+  }, // Recommended transports for production
 })
 
 // Optional: Add listeners for global connection status
