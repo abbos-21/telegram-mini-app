bf4e776a856817ff1e9ba98f202fbb1f2ab4863c
abbos-21 <abbos.nazariy0621@gmail.com>
2025-10-26 21:29:41 +0500

    commit 47

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from 'axios'
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
