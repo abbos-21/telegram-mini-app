faa802afff9c84b2ceb061b9e367c96f60aae08a
abbos-21 <abbos.nazariy0621@gmail.com>
2025-11-10 15:38:42 +0500

    commit 116

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,23 @@ import { isMusicPlaying, isMusicEnabled, setMusicPlaying, setMusicAvailable } fr
 import { toast } from 'vue3-toastify'
 
 import LoaderComponent from './components/LoaderComponent.vue'
-import { useAdsgram } from '@adsgram/vue'
+// import { useAdsgram } from '@adsgram/vue'
 
-const blockId = import.meta.env.VITE_BLOCK_ID
+// const blockId = import.meta.env.VITE_BLOCK_ID
 
-const { show, addEventListener } = useAdsgram({
-  blockId,
-})
+// const { show, addEventListener } = useAdsgram({
+//   blockId,
+// })
 
-const startAdLoop = async () => {
-  try {
-    await show()
-  } catch (err) {
-    console.log('Error in ad loop: ', err)
-  } finally {
-    setTimeout(startAdLoop, 180000) // every 3 minutes
-  }
-}
+// const startAdLoop = async () => {
+//   try {
+//     await show()
+//   } catch (err) {
+//     console.log('Error in ad loop: ', err)
+//   } finally {
+//     setTimeout(startAdLoop, 180000) // every 3 minutes
+//   }
+// }
 
 addEventListener('onBannerNotFound', () => {
   console.log('No ad available at the moment')
@@ -92,7 +92,6 @@ const authenticate = async () => {
 
 onMounted(async () => {
   await authenticate()
-  await startAdLoop()
 })
 </script>
 
